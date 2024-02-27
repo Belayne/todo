e6743ee92713d66e67167fcdcd35cb2bab60d171
Naol <37399395+Belayne@users.noreply.github.com>
2024-02-27 14:41:03 +0100

    Add createTodo function

---
+++ src/todo.js
@@ -0,0 +1,32 @@
+export default function createTodo(task, notes = "", priority = 0, dueDate = "") {
+    function changeTask(newTask){
+        task = newTask
+    }
+
+    function changeNotes(newNotes){
+        notes = newNotes;
+    }
+
+    function changePriority(newPriority){
+        priority = newPriority;
+    }
+
+    function changeDate(newDate){
+        dueDate = newDate;
+    }
+
+    const getTodo = () => {
+        task,
+        notes,
+        priority,
+        dueDate
+    }
+
+    return {
+        getTodo,
+        changeTask,
+        changeDate,
+        changeNotes,
+        changePriority
+    }
+}
